b51ffdac78a777dd406b132d757566f87fb76bd5
Maryna <marinaslabeniak@gmail.com>
2025-11-23 19:49:18 +0200

    Lab2 Test Dudles

--- test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/exercise4/FetchUserProfileUseCaseSyncTest.java
+++ test_doubles_fundamentals/src/test/java/com/techyourchance/testdoublesfundamentals/exercise4/FetchUserProfileUseCaseSyncTest.java
@@ -1,5 +1,31 @@
-package com.techyourchance.testdoublesfundamentals.exercise4;
+package com.techyourchance.testdoublesfundamentals.exercise4.networking;
 
-public class FetchUserProfileUseCaseSyncTest {
+import com.techyourchance.testdoublesfundamentals.example4.networking.NetworkErrorException;
+import com.techyourchance.testdoublesfundamentals.exercise4.networking.UserProfileHttpEndpointSync.EndpointResult;
+import com.techyourchance.testdoublesfundamentals.exercise4.networking.UserProfileHttpEndpointSync.EndpointResultStatus;
 
+public class UserProfileHttpEndpointSyncTD implements UserProfileHttpEndpointSync{
+private String mUserId;
+private EndpointResult mEndpointResult;
+private NetworkErrorException mNetworkErrorException;
+public void setEndpointResult(EndpointResult endpointResult){
+mEndpointResult = endpointResult;
+}
+public void setNetworkErrorException(NetworkErrorException networkErrorException){
+mNetworkErrorException = networkErrorException;
+}
+@Override
+public EndpointResult getUserProfile(String userId) throws NetworkErrorException{
+mUserId = userId;
+if(mNetworkErrorException != null){
+throw mNetworkErrorException;
+}
+if(mEndpointResult == null){
+return new EndpointResult(EndpointResultStatus.SUCCESS, "John Doe", "http://example.com/image.png");
+}
+return mEndpointResult;
+}
+public String getUserId(){
+return mUserId;
+}
 }
